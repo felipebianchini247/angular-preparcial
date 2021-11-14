a3e9806671b807ff3ca9afa3667f52c55b73af09
felipebianchini247 <felipebianchini247@gmail.com>
2021-11-14 19:48:34 -0300

    4 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { SaludoComponent } from './saludo/saludo.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HelloComponent, SaludoComponent],
+  bootstrap: [SaludoComponent, AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
