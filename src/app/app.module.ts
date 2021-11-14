bcf773c69cd3d37635d3758718da89c138d98a20
felipebianchini247 <felipebianchini247@gmail.com>
2021-11-14 20:59:38 -0300

    3 files modified, 6 added, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { SaludoComponent } from './saludo/saludo.component';
+import { SaludoComponent } from './components/saludo/saludo.component';
+import { ListaArticulosComponent } from './components/lista-articulos/lista-articulos.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, SaludoComponent],
-  bootstrap: [SaludoComponent, AppComponent],
+
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    SaludoComponent,
+    ListaArticulosComponent,
+  ],
+
+  bootstrap: [AppComponent, ListaArticulosComponent],
 })
 export class AppModule {}
---
+++ src/app/components/lista-articulos/lista-articulos.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-articulos',
+  templateUrl: './lista-articulos.component.html',
+  styleUrls: ['./lista-articulos.component.css'],
+})
+export class ListaArticulosComponent implements OnInit {
+  titulo = 'Lista de articulos';
+
+  constructor() {}
+
+  ngOnInit() {}
+}
