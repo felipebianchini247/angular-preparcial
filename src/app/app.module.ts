d611207a824f5ff18969b58a59481b9f198eb46f
felipebianchini247 <felipebianchini247@gmail.com>
2021-11-15 15:31:43 -0300

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { ListaArticulosComponent } from './components/lista-articulos/lista-articulos.component';
 import { ConvertirAEspacioPipe } from './shared/convertir-aespacio.pipe';
+import { PuntajeComponent } from './components/puntaje/puntaje.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
@@ -15,6 +16,7 @@ import { ConvertirAEspacioPipe } from './shared/convertir-aespacio.pipe';
     HelloComponent,
     ListaArticulosComponent,
     ConvertirAEspacioPipe,
+    PuntajeComponent,
   ],
 
   bootstrap: [AppComponent, ListaArticulosComponent],
--- src/app/components/puntaje/puntaje.component.ts
+++ src/app/components/puntaje/puntaje.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-puntaje',
   templateUrl: './puntaje.component.html',
-  styleUrls: ['./puntaje.component.css']
+  styleUrls: ['./puntaje.component.css'],
 })
 export class PuntajeComponent implements OnInit {
+  puntaje: number = 4;
+  puntajeAncho: number;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  ngOnChanges(): void {
+    this.puntajeAncho = (this.puntaje * 68) / 5;
   }
-
 }
