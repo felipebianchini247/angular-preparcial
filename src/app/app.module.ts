1614c9a38d27dbd549713ca6bb3d5008a202078f
felipebianchini247 <felipebianchini247@gmail.com>
2021-11-15 01:35:20 -0300

    3 files modified, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { SaludoComponent } from './components/saludo/saludo.component';
 import { ListaArticulosComponent } from './components/lista-articulos/lista-articulos.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
 
-  declarations: [
-    AppComponent,
-    HelloComponent,
-    SaludoComponent,
-    ListaArticulosComponent,
-  ],
+  declarations: [AppComponent, HelloComponent, ListaArticulosComponent],
 
   bootstrap: [AppComponent, ListaArticulosComponent],
 })
--- src/app/components/lista-articulos/lista-articulos.component.ts
+++ src/app/components/lista-articulos/lista-articulos.component.ts
@@ -1,4 +1,6 @@
+import { VERSION } from '@angular/compiler/src/version';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-lista-articulos',
@@ -7,8 +9,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaArticulosComponent implements OnInit {
   titulo = 'Lista de articulos';
+  alturaImagen = 40;
+  filtro: string;
+
+  articulos: any[] = [
+    {
+      id: 2,
+      descripcion: 'Articulo X',
+      codigo: 'xsd-143',
+      cantidad: 139,
+      precio: 1221.4,
+      puntaje: 4,
+      imagen: 'https://i.blogs.es/a5a58f/1366_2000/1366_2000.png',
+      active: true,
+    },
+    {
+      id: 5,
+      descripcion: 'Articulo Y',
+      codigo: 'dlg-912',
+      cantidad: 336,
+      precio: 400.99,
+      puntaje: 3,
+      imagen: 'https://m.media-amazon.com/images/I/61B0qTsNumL._AC_SX522_.jpg',
+      active: false,
+    },
+  ];
 
   constructor() {}
 
   ngOnInit() {}
+
+  obtenerVersion(): string {
+    return VERSION.full;
+  }
+
+  limpiar(): void {
+    this.articulos = [];
+    this.filtro = '';
+  }
+
+  cambiarEstado(art): void {
+    art.active = !art.active;
+  }
 }
